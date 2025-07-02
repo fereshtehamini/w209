d046900c3999c0f60b19b9d512600ed943371a2b
Fereshteh Amini <aminife@gmail.com>
2025-07-02 12:02:48 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def w209():
-    file='visualization.vl.json'
+    file='AfricaPopulationMap_VLSpec.json'
     return render_template('w209.html',file=file)
 
 if __name__ == '__main__':
